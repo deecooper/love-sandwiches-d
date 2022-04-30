9393e21e55e84f8c4b71d28fb748a4bb69e7dd91
deecooper <deecoopermedia@gmail.com>
2022-04-30 10:16:08 +0000

    Retrieve latest stock numbers from spreadsheet

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 import gspread
 from google.oauth2.service_account import Credentials
+from pprint import pprint
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
@@ -15,6 +16,9 @@ SHEET = GSPREAD_CLIENT.open('love_sandwiches_dee')
 def get_sales_data():
     """
     Get sales figures input from the user.
+    Run a while loop to collect a valid string of data from the user
+    via the terminal, which must be a string of 6 numbers separated
+    by commas. The loop will repeatedly request data, until it is valid.
     """
     while True:
         print("Please enter sales data from the last market.")
@@ -49,16 +53,43 @@ def validate_data(values):
         return False
 
     return True
+
+
 def update_sales_worksheet(data):
     """
-    update sales worksheet, add new row with the list data provided.
-
+    Update sales worksheet, add new row with the list data provided
     """
     print("Updating sales worksheet...\n")
     sales_worksheet = SHEET.worksheet("sales")
     sales_worksheet.append_row(data)
-    print("Sales worksheet updates successfully.\n")
+    print("Sales worksheet updated successfully.\n")
 
-data = get_sales_data()
-sales_data = [int(num) for num in data]
-update_sales_worksheet(sales_data)
+def calculate_surplus_data(sales_row):
+    """
+    This function calculates the surplus data for each item type.
+    sales figure subtracted from stick gives us the surplus
+    postive indicates waste
+    negative indicates extra sock made when stock ran out
+
+    """
+
+
+    print("Calculating surplus data....\n")
+    stock = SHEET.worksheet("stock").get_all_values()
+    stock_row = stock[-1]
+    print(stock_row)
+
+
+
+
+
+
+
+def main():
+    data = get_sales_data()
+    sales_data = [int(num) for num in data]
+    update_sales_worksheet(sales_data)
+    calculate_surplus_data(sales_data)
+
+print("Welcome to Love Sandwiches")
+main()
